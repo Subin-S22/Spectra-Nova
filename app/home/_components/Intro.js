f010863f781ce988a7d46cc4c20a28a1fab74799
Subin-S22 <subin.s22@outlook.com>
2024-12-30 22:46:52 +0530

    chore: Home responsive

--- app/home/_components/Intro.js
+++ app/home/_components/Intro.js
@@ -2,10 +2,10 @@ import HeaderSubSection from "@/components/HeaderSubSection"
 
 export default function Intro() {
   return <div className="flex gap-4 flex-col md:flex-row bg-card-bg p-8 sm:p-16 rounded-md">
-    <div className="text-white text-4xl basis-2/3">
+    <div className="text-white text-3xl basis-2/3">
       Your <span className="text-text-color">TRUSTED</span> partner in
       innovative software solutions, We transform ideas into
-      <span className="text-text-color">POWERFUL DIGITAL PRODUCTS.</span>
+      <span className="text-text-color text-wrap"> POWERFUL DIGITAL PRODUCTS.</span>
     </div>
     <HeaderSubSection />
   </div>
